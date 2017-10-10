73368718b6bedb5e941bb4ed033ff28522a8c48e
Sandeep Raju Prabhakar <me@sandeepraju.in>
2017-10-10 08:34:51 -0700

    adds async server example

---
+++ javascript/async_server.js
@@ -0,0 +1,25 @@
+var net = require('net');
+var http = require('http');
+
+// Start a TCP Server
+net.createServer(function (socket) {
+
+  // Handle incoming messages from clients.
+  socket.on('data', function (data) {
+    console.log("Request came in for ", data);
+
+    // Do something CPU intensive
+    for (var i = 0; i < 100000; i++)
+      for (var j = 0; j < 100000; j++);
+    socket.write("response");
+
+    // Do something I/O bound
+    http.get("http://httpbin.org/delay/1", function(res) {
+      // Return the response back to the client
+      socket.write("response");
+    });
+  });
+
+}).listen(9999);
+
+console.log("Server running at port localhost:9999\n");
